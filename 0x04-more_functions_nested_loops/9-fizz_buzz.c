7c126c2bf945375d219bbfbd268eed03b4e39604
Joshua <joshuaogunlade1@gmail.com>
2022-09-16 17:08:38 -0700

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,33 +12,29 @@ int main(void)
 {
 	int num = 1;
 
-	while (num++ < 100)
+	while (num <= 100)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (num % 3 == 0) && (num % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
-		else if ((num % 3) == 0)
+		else if (num % 5 == 0)
 		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
+			if (num == 100)
 			{
 				printf("Buzz ");
+				print("\n");
 			}
 			else
 			{
 				printf("Buzz ");
 			}
 		}
+		else if (num % 3 == 0)
+			printf("Fizz");
 		else
-		{
 			printf("%d ", num);
-		}
+	num++;
 	}
-	printf("\n");
-
-	return (0);
+return (0);
 }
