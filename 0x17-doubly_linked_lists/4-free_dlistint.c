91f0a27a93c3360b49fa886d2c8a0f897aaa1edc
Joshua <joshuaogunlade1@gmail.com>
2022-12-08 18:13:18 -0800

    new commit

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/** 
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: a pointer to the head node
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	while (temp->next)
+	{
+		temp = temp->next;
+		free (temp->prev);
+	}
+	free(temp);
+}
